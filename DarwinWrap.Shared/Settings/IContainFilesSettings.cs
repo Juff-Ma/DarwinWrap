a5b925e9a802ed201afe0b7b0e1ce18018c04f20
Julian Rossbach <contact@juffma.de>
2026-04-05 17:32:25 +0200

    Add file settings provider

---
+++ DarwinWrap.Shared/Settings/IContainFilesSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace DarwinWrap.Shared.Settings;
+
+public interface IContainFilesSettings
+{
+    /// <summary>
+    /// Path to the files to be included in the package.
+    /// If this is a directory, all files in the directory will be included.
+    /// If this is a zip file, all files in the zip will be included.
+    /// </summary>
+    string? FilesPath { get; set; }
+}
